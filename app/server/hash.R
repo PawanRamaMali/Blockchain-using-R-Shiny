6762473dd32034395104d401492d5d92d5f75d75
Pawan Rama Mali <prm@outlook.in>
2021-11-29 19:36:26 +0530

    Update hash.R

--- app/server/hash.R
+++ app/server/hash.R
@@ -2,26 +2,26 @@ find_hashed_output <- eventReactive(input$start_hash, {
   digest::digest(input$text_to_hash,
                  algo = input$select_hash,
                  serialize = FALSE)
-  
-  
-  
-  
+
+
+
+
 })
 
-output$hashed_output <- renderText({
+ <- renderText({
   find_hashed_output()
 })
 
 
 observeEvent(input$start_hash, {
-  output_clip <- digest::digest(input$text_to_hash,
+  input$hashed_output <- digest::digest(input$text_to_hash,
                                 algo = input$select_hash,
                                 serialize = FALSE)
   output$clip <- renderUI({
     rclipButton(
       "clipbtn",
       " ",
-      output_clip,
+      input$hashed_output,
       icon = icon("clipboard")
     )
   })
